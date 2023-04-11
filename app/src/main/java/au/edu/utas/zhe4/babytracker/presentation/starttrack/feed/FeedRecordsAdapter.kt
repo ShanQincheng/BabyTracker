eb8467d3e83b7d3c4408fd5adb76d216ea967491
shanqincheng <zl.jesse.he@gmail.com>
2023-04-11 13:41:21 +1000

    unfinished: code refactoring view model

--- app/src/main/java/au/edu/utas/zhe4/babytracker/presentation/starttrack/feed/FeedRecordsAdapter.kt
+++ app/src/main/java/au/edu/utas/zhe4/babytracker/presentation/starttrack/feed/FeedRecordsAdapter.kt
@@ -1,27 +1,41 @@
 package au.edu.utas.zhe4.babytracker.presentation.starttrack.feed
 
+import android.content.Context
 import android.content.Intent
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import au.edu.utas.zhe4.babytracker.FEEDING_RECORD_INDEX
 import au.edu.utas.zhe4.babytracker.databinding.FeedRecordBinding
 import au.edu.utas.zhe4.babytracker.entities.Feed
 import au.edu.utas.zhe4.babytracker.utils.LongToLocalDateTimeString
 
-class FeedStartTrackAdapter(private val records: MutableList<Feed>) :
-    RecyclerView.Adapter<FeedStartTrackHolder>()
+class FeedRecordsAdapter(
+    private val records: MutableList<Feed>,
+    private val context: Context,
+) :
+    RecyclerView.Adapter<FeedRecordsAdapter.ViewHolder>()
 {
+
+    class ViewHolder(val ui: FeedRecordBinding) : RecyclerView.ViewHolder(ui.root) {
+        val feedingTypeView: TextView = ui.tvFeedingType
+        val feedingSideView: TextView = ui.tvFeedingSide
+        val feedingNote: TextView = ui.tvFeedingNote
+        val feedingTime: TextView = ui.tvFeedingTime
+    }
+
     // Inflates a new row, and wraps it in a new ViewHolder( FeedingRecordHolder )
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeedStartTrackHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         //inflate a new row from the activity_feed_start_track.xml
         val ui = FeedRecordBinding.inflate(
-            layoutInflater, parent, false)
-        return FeedStartTrackHolder(ui)
+            LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(ui)
     }
 
     // Populates each row data as required. First we get the data out of the array,
     // then we set the TextView in the row:
-    override fun onBindViewHolder(holder: FeedStartTrackHolder, position: Int) {
+    override fun onBindViewHolder(holder: FeedRecordsAdapter.ViewHolder, position: Int) {
         val record = records[position]
         holder.ui.tvFeedingType.text = record.type.toString()
         holder.ui.tvFeedingSide.text = record.side.toString()
@@ -32,7 +46,7 @@ class FeedStartTrackAdapter(private val records: MutableList<Feed>) :
             val i = Intent(holder.ui.root.context,
                 au.edu.utas.zhe4.babytracker.Feed::class.java)
             i.putExtra(FEEDING_RECORD_INDEX, position)
-            startActivity(i)
+            context.startActivity(i)
         }
     }
 
--- app/src/main/java/au/edu/utas/zhe4/babytracker/presentation/starttrack/feed/FeedStartTrackViewModel.kt
+++ app/src/main/java/au/edu/utas/zhe4/babytracker/presentation/starttrack/feed/FeedStartTrackViewModel.kt
@@ -1,4 +1,13 @@
 package au.edu.utas.zhe4.babytracker.presentation.starttrack.feed
 
-class FeedStartTrackViewModel {
+import android.app.Application
+import androidx.recyclerview.widget.LinearLayoutManager
+import au.edu.utas.zhe4.babytracker.framework.BabyTrackerViewModel
+import au.edu.utas.zhe4.babytracker.framework.UseCases
+
+class FeedStartTrackViewModel(application: Application, usecases: UseCases) : BabyTrackerViewModel
+    (application, usecases)
+{
+
+
 }
